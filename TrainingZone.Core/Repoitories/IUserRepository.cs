4b6ae4a738c89ecefedbfbd592a559ccc2dcfff7
Miqayel Avagyan <miqayel.avagyan@outlook.com>
2019-03-29 19:03:35 +0400

    added user repositories

---
+++ TrainingZone.Core/Repoitories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingZone.Core.Auth.Users;
+
+namespace TrainingZone.Core.Repoitories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> Create(string firstName, string lastName, string email, string userName, string password);
+        Task<User> FindByName(string userName);
+        Task<bool> CheckPassword(User user, string password);
+    }
+}
