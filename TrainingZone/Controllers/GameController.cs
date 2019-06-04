186ec74dd9bc86177f9d2cadc43e6c8d63c991a8
Miqayel Avagyan <miqayel.avagyan@outlook.com>
2019-06-04 18:40:35 +0400

    Added game Controller

---
+++ TrainingZone/Controllers/GameController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrainingZone.Core.Auth.Users;
+using TrainingZone.Core.Interfaces;
+
+namespace TrainingZone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IGameRepository _scoreRepository
+        private readonly IScoreRepository _scoreRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+
+        public GameController(UserManager<User> userManager, IScoreRepository scoreRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _scoreRepository = scoreRepository;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        // GET: api/Game
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Game/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Game
+        [HttpPost]
+        public void Create([FromBody] string value)
+        {
+
+        }
+
+        // PUT: api/Game/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- TrainingZone/Hubs/GameHub.cs
+++ TrainingZone/Hubs/GameHub.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TrainingZone.Core.Auth.Users;
 using TrainingZone.Core.Interfaces;
 
 namespace TrainingZone.Hubs
@@ -12,16 +14,17 @@ namespace TrainingZone.Hubs
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class GameHub : Hub
     {
-        private readonly IScoreRepository _scoreRepository;
-        public GameHub(IScoreRepository scoreRepository)
+        private readonly IGameRepository _gameRepository;
+        private readonly UserManager<User> _userManager;
+        public GameHub(IGameRepository gameRepository, UserManager<User> userManager)
         {
-            _scoreRepository = scoreRepository;
+            _gameRepository = gameRepository;
+            _userManager = userManager;
         }
 
         public async Task SendToAll(string userId, string message)
         {
             var currentUserId = Context.UserIdentifier;
-            var scors = await _scoreRepository.GetByPlayerId(userId);
             var name = Context.User.Identity.Name;
             await Clients.User(userId).SendAsync("sendToAll", name, message);
         }
