92bab9baf205630d1503a25cf45078d5b299dd24
Miqayel Avagyan <miqayel.avagyan@outlook.com>
2019-06-05 18:29:35 +0400

    added game controller

--- TrainingZone/Controllers/GameController.cs
+++ TrainingZone/Controllers/GameController.cs
@@ -16,16 +16,15 @@ namespace TrainingZone.Controllers
     public class GameController : ControllerBase
     {
         private readonly IGameRepository _gameRepository;
-        private readonly IScoreRepository _scoreRepository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly UserManager<User> _userManager;
         private readonly IMapper _mapper;
 
-        public GameController(UserManager<User> userManager, IScoreRepository scoreRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        public GameController(UserManager<User> userManager, IUnitOfWork unitOfWork, IMapper mapper, IGameRepository gameRepository)
         {
-            _scoreRepository = scoreRepository;
             _userManager = userManager;
             _unitOfWork = unitOfWork;
+            _gameRepository = gameRepository;
             _mapper = mapper;
         }
         // GET: api/Game
@@ -36,7 +35,7 @@ namespace TrainingZone.Controllers
         }
 
         // GET: api/Game/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
@@ -44,7 +43,7 @@ namespace TrainingZone.Controllers
 
         // POST: api/Game
         [HttpPost]
-        public void Create([FromBody] string value)
+        public void Create([FromBody] CreateGameRequest request)
         {
 
         }
---
+++ TrainingZone/Models/Requests/GameRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingZone.Models.Requests
+{
+    public class GameRequest
+    {
+        public string FirstPlayerId { get; set; }
+        public int MatrixSize { get; set; }
+        public char FirstPlayerTurnType { get; set; }
+        public char SecondPlayerTurnType { get; set; }
+        public int? FirstPlayedPlayer { get; set; }
+        public bool IsGameStarted { get; set; }
+        public bool IsGameFinished { get; set; }
+        public virtual ICollection<Tuple<int,int>> PlayedCoordinates { get; set; }
+    }
+}
